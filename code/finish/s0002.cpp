ba7e240bb18a2a1a70a8a5167ff4cbd7af404a12
simbafs <simba.fs@gmail.com>
2018-12-28 23:52:34 +0800

    change

---
+++ code/finish/s0002.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+char tab(char a){
+	switch(a){
+		case 'E':
+			return '3';
+		case 'J':
+			return 'L';
+		case 'S':
+			return '2';
+		case 'Z':
+			return '5';
+	}
+	return a;
+}
+
+int mirror(char a,char b){
+	if ( a == b ){
+		switch(a){
+			case 'A':
+			case 'H':
+			case 'I':
+			case 'M':
+			case 'O':
+			case 'T':
+			case 'U':
+			case 'V':
+			case 'W':
+			case 'X':
+			case 'Y':
+			case '1':
+			case '8':
+				return 1;
+			default:
+				return 0;
+		}
+	}
+	return 0;
+}
+
+int main(){
+	//cout << tab('Z'') << ' ' << tab('5');
+	
+	
+	string a;
+	cin >> a;
+	while(1){
+		bool flag=true;
+		bool mirrored=true;	
+		bool strictmirror=true;
+		for(int i=0,j=a.length()-1;i<=j;i++,j--){
+			if( a[i] != a[j] ){
+				flag = false;
+			}
+			if( tab(a[i]) != tab(a[j]) ){
+				//cout << i << ' ' << j << endl;
+				mirrored = false;
+			}
+			if( mirror(a[i],a[j]) != 1 ){
+				strictmirror = false;
+			}
+		}
+		cout << a;
+		if( strictmirror ){
+			cout << " -- is a mirrored palindrome.";
+		}else if( mirrored && !flag ){
+			cout << " -- is a mirrored string.";
+		}else if( flag ){
+			cout << " -- is a regular palindrome.";
+		}else{
+			cout << " -- is not a palindrome.";
+		}
+		if(cin >> a){
+			cout << endl << endl;
+		}else{
+			cout << endl;
+			break;
+		}
+	}
+	return 0;
+	
+}
--- code/working/s0001.cpp
+++ code/working/s0001.cpp
@@ -1,64 +1,16 @@
 #include <iostream>
-#include <string>
-
 using namespace std;
-
-char tab(char a){
-	switch(a){
-		case 'E':
-			return '3';
-		case 'J':
-			return 'L';
-		case 'S':
-			return '2';
-		case 'z':
-			return '5';
-		default:
-			return a;
-	}
-}
-
-int mirror(char a,char b){
-	if ( a == b ){
-		switch(a){
-			case 'A':
-			case 'H':
-			case 'I':
-			case 'M':
-			case 'O':
-			case 'T':
-			case 'U':
-			case 'V':
-			case 'W':
-			case 'X':
-			case 'Y':
-			case '1':
-			case '8':
-				return 1;
-		}
-	}else if( tab(a) == tab(b) ){
-		return 1;
-	}
-	return 0;
-}
-
 int main(){
-	string a;
-	while(cin >> a){
-		bool flag=true;
-		bool flagmirror=true;
-		for(int i=0,j=a.length()-1;i<=j;i++,j--){
-			if (tab(a[i]) != tab(a[j])) flag = false;
-			if (!mirror(a[i],a[j])) flagmirror = false;
-		}
-		
-		if (flagmirror){
-			cout << a << " -- is a mirrored string." << endl << endl;
-		}else if(flag){
-			cout << a << " -- is a regular palindrome." << endl << endl;
-		}else{
-			cout << a << " -- is not a palindrome." << endl << endl;
-		}
+	int n=6;
+	int k=5;
+	int w=1;
+	int all=0;
+	cin >> n >> k >> w;
+	all=n;
+	while(n>=k){
+		n+=w-k;
+		all+=w;
 	}
+	cout << all << endl;
 	return 0;
 }
