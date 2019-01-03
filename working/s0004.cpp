5963d3e0c5eb83d63dc7fae472b09a50caaabdc1
Simba <39305460+simbafs@users.noreply.github.com>
2019-01-03 10:01:56 +0800

    Update s0004.cpp

--- working/s0004.cpp
+++ working/s0004.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 
 using namespace std;
@@ -11,14 +12,13 @@ int main(){
 	while(cin >> num){
 		bool below = true;
 		bool above = true;
-		int* minpass = score;
-		int* maxnopass;
+		int minpass = num-1;
+		int maxnopass = 0;
 		for(int i=0; i<num; i++){
 			cin >> score[i];
 			if(score[i] < 60) above = false;
 			if(score[i] >= 60) below = false;
 		}
-
 		sort();
 		for(int i=0; i<num; i++){
 			cout << score[i];
@@ -28,22 +28,22 @@ int main(){
 				cout << endl;
 			}
 		}
-		cout << *minpass << ' ' << *maxnopass << endl;
-		while(*minpass < 60){
-			minpass ++;
-		}
-		maxnopass = minpass -1;
-		cout << *minpass << ' ' << *maxnopass << endl;
-		cout << minpass-score << ' ' << maxnopass-score << endl;
-/*
+		
+		while(score[minpass-1] > 60 && minpass-1 >= 0) minpass --;
+//		cout << minpass << ' ' << maxnopass << endl;
+		while(score[maxnopass+1] < 60 && maxnopass+1 < num) maxnopass ++;
+//		cout << minpass << ' ' << maxnopass << endl;
+		
 		if(above && !below){
+			cout << score[minpass] << endl;
 			cout << "best case" << endl;
 		}else if(!above && below){
+			cout << score[maxnopass] << endl;
 			cout << "worst case" << endl;
 		}else{
-
+			cout << score[maxnopass] << endl << score[minpass] << endl;
 		}
-*/
+
 	}
 	return 0;
 }
