6e4d45585a34dfd6fa81c191d18ee386e0ee9438
simbafs <simba.fs@gmail.com>
2019-01-07 18:05:11 +0800

    change something

--- working/0006.cpp
+++ working/0006.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
-#include <cmath>
+#include <math.h>
 
 using namespace std;
 
-int sort();
+int sort(int*,int);
 
 int main(){
-	
+	int n = 0;
+	int m = 0;
+	while(cin >> n >> m){
+		int k[65535] = {};
+		int index = 0;
+		int sum = 0;
+		for(; index<m; index++) cin >> k[index];
+		sort(k,index);
+		for(int i=0; i<n; i++){
+			int* ki = k;
+			if(i != *ki){
+				sum += pow(2.0,n-i);
+			}
+		}
+		cout << sum << endl;
+
+	}
+	return 0;
 }
 
 int sort(int ar[],int num){
